12000862abb76670f5b24e6274c0f062f64decf6
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-01-27 00:30:29 +0530

    nav

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,36 @@
-//import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { userdelete } from "../store/reducers/UserReducers";
 
 const Users = () => {
-  return (
-    <div>
-      userss
-    </div>
-  )
-}
+  const { users } = useSelector((state) => state.UserReducer);
+  const dispatch = useDispatch();
 
-export default Users
+  const DeleteHandler = (index) => {
+    dispatch(userdelete(index));
+  };
+
+  return (
+    <div className="m-auto container p-10 mt-5 bg-red-100">
+      <h1 className="text-2xl font-bold text-red-900"> User List</h1>
+      <ul>
+        {users.map((user, index) => {
+          return (
+            <li key={user.id}>
+              <h1>
+                {user.name}{" "}
+                <span
+                  onClick={() => DeleteHandler(index)}
+                  className="text-red-500 font-black cursor-pointer"
+                >
+                  X
+                </span>
+              </h1>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
